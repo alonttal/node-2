171b223bd74e167e82beeefc0324cc205518a387
Alon <alontalmor@gmail.com>
2017-12-01 00:26:38 +0200

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,33 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$dgBcngA.NncZ/laHSXVOAuybdM4bnfK0fNKrTx6.crKoxBpZb5GgC';
+var hashedPassword2 = '$2a$10$doHFaRDKR9ar0iU.MMYtqehUcZqsXFyWpgO1vUG6XkcmYzmUh9HpS';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+bcrypt.compare(password, hashedPassword2, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
  
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
